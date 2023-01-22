0273948b01714e57627d143f51785e632ab8678b
Petar Petrov <petrov89_stz1@abv.bg>
2023-01-22 17:00:59 +0200

    Add tasks for More Exercises

---
+++ 6.Data Types and Variables - More Exercises/P03_FloatingEquality.java
@@ -0,0 +1,28 @@
+package MoreExersises02_DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class P03_FloatingEquality {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double first = Double.parseDouble(scanner.nextLine());
+        double second = Double.parseDouble(scanner.nextLine());
+        double difference = 0;
+        if (first < 0 && second < 0){
+            difference = Math.abs(first - second);
+        } else if (first < 0){
+            difference = first + second;
+        } else if (second < 0){
+            difference = second + first;
+        } else {
+            difference = Math.abs(first - second);
+        }
+        double eps = 0.0000001;
+
+        if (difference < eps){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
---
+++ 6.Data Types and Variables - More Exercises/P04_RefactoringPrimeChecker.java
@@ -0,0 +1,21 @@
+package MoreExersises02_DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class P04_RefactoringPrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numbers = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 2; i <= numbers; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            System.out.printf("%d -> %b%n", i, isPrime);
+        }
+    }
+}
